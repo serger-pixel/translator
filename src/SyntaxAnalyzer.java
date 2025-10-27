3506de3370fe8cd64e0eb1a6a582a4fadaa3fab9
Sergey <rihlerstudy@gmail.com>
2025-10-27 06:41:18 +0300

    Добавлены символы для определения пустых и непустых тактов

--- src/SyntaxAnalyzer.java
+++ src/SyntaxAnalyzer.java
@@ -4,6 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 public  class SyntaxAnalyzer {
+    //Пустой таккт
+    private static String emptyProcess = "-";
+    //Обычный такт
+    private static String fullProcess = "+";
+
+
     //Таблица нетерминалов
     private static LinkedHashMap<String, String> notTerminals = new LinkedHashMap<>(Map.ofEntries(
             Map.entry("A", "<Цифра>"),
@@ -31,40 +37,33 @@ public  class SyntaxAnalyzer {
             Map.entry("W", "<Список выбора'>")
     ));
 
-    //Группы правил
-    private enum GROUPS{
-        FIRST,
-        SECOND,
-        THIRD,
-        FOURTH
-    }
-
-
     //Таблица переходов
     private static LinkedHashMap<String, List<String>> jumpTable = new LinkedHashMap<>();
 
     private static void createJumpTable(){
         char[] operators = {'+', '-', '/'};
         //1-ая группа
-        jumpTable.put("BEGIN,K", List.of( "END", "O"));
-        jumpTable.put("VAR,J", List.of( ";INTEGER:", "L"));
-        jumpTable.put(",,M", List.of( "M", "D", ","));
-        jumpTable.put("(,T", List.of( ")", "Q", "("));
-        jumpTable.put("WRITE,P", List.of( ";", "L"));
-        jumpTable.put("READ,P", List.of( ";", "L"));
-        jumpTable.put("CASE,P", List.of( ";END_CASE", "V", "OF", "Q"));
-        jumpTable.put("~,F", List.of( ));
-        for (char op : operators){
-            jumpTable.put(op+",G", List.of( ));
+        jumpTable.put("BEGIN,K", List.of("+", "END", "O"));
+        jumpTable.put("VAR,J", List.of("+", ";INTEGER:", "L"));
+        jumpTable.put(",,M", List.of("+", "M", "D", ","));
+        jumpTable.put("(,T", List.of("+", ")", "Q", "("));
+        jumpTable.put("WRITE,P", List.of("+", ";", "L"));
+        jumpTable.put("READ,P", List.of("+", ";", "L"));
+        jumpTable.put("CASE,P", List.of("+", ";END_CASE", "V", "OF", "Q"));
+        jumpTable.put("~,F", List.of("+"));
+
+        for (char op : operators) {
+            jumpTable.put(op + ",G", List.of("+"));
         }
 
         for (char c = '0'; c <= '9'; c++) {
-            jumpTable.put(c + ",A", List.of( ));
+            jumpTable.put(c + ",A", List.of("+"));
         }
 
         for (char c = 'a'; c <= 'z'; c++) {
-            jumpTable.put(c + ",H", List.of());
+            jumpTable.put(c + ",H", List.of("+"));
         }
+
 
 
         //2-ая группа
@@ -72,161 +71,167 @@ public  class SyntaxAnalyzer {
 
         // Переходы для <Конст'>
         for (char c = '0'; c <= '9'; c++) {
-            jumpTable.put(c + ",C", List.of( "C", "A"));
+            jumpTable.put(c + ",C", List.of("-", "C", "A"));
         }
 
 
         // Переходы для <Конст>
         for (char c = '0'; c <= '9'; c++) {
-            jumpTable.put(c + ",B", List.of( "C", "A"));
+            jumpTable.put(c + ",B", List.of("-", "C", "A"));
         }
 
 
         // Переходы для <Идент'>
         for (char c = 'a'; c <= 'z'; c++) {
-            jumpTable.put(c + ",E", List.of( "H", "E"));
+            jumpTable.put(c + ",E", List.of("-", "H", "E"));
         }
 
 
         // Переходы для <Идент>
         for (char c = 'a'; c <= 'z'; c++) {
-            jumpTable.put(c + ",D", List.of( "H", "E"));
+            jumpTable.put(c + ",D", List.of("-", "H", "E"));
         }
 
 
         // Переходы для <Выбор>
         for (char c = '0'; c <= '9'; c++) {
-            jumpTable.put(c + ",U", List.of(";", "N", ":", "B"));
+            jumpTable.put(c + ",U", List.of("-", ";", "N", ":", "B"));
         }
 
 
         // Переходы для <Список выбора'>
         for (char c = '0'; c <= '9'; c++) {
-            jumpTable.put(c + ",W", List.of("W", "U"));
+            jumpTable.put(c + ",W", List.of("-", "W", "U"));
         }
 
 
         // Переходы для <Список выбора>
         for (char c = '0'; c <= '9'; c++) {
-            jumpTable.put(c + ",V", List.of("W", "U"));
+            jumpTable.put(c + ",V", List.of("-", "W", "U"));
         }
 
 
         // Переходы для <Оператор>
         for (char c = 'a'; c <= 'z'; c++) {
-            jumpTable.put(c + ",P", List.of("N"));
+            jumpTable.put(c + ",P", List.of("-", "N"));
         }
 
 
         // Переходы для <Операнд'>
         for (char c = 'a'; c <= 'z'; c++) {
-            jumpTable.put(c + ",T", List.of("D"));
+            jumpTable.put(c + ",T", List.of("-", "D"));
         }
         for (char c = '0'; c <= '9'; c++) {
-            jumpTable.put(c + ",T", List.of("B"));
+            jumpTable.put(c + ",T", List.of("-", "B"));
         }
-        jumpTable.put("(,T", List.of("T"));
+        jumpTable.put("(,T", List.of("-", "T"));
+
 
         // Переходы для <Операнд>
-        jumpTable.put("~,S", List.of("T", "F"));
+        jumpTable.put("~,S", List.of("-", "T", "F"));
         for (char c = 'a'; c <= 'z'; c++) {
-            jumpTable.put(c + ",S", List.of("T"));
+            jumpTable.put(c + ",S", List.of("-", "T"));
         }
         for (char c = '0'; c <= '9'; c++) {
-            jumpTable.put(c + ",S", List.of("T"));
+            jumpTable.put(c + ",S", List.of("-", "T"));
         }
-        jumpTable.put("(,S", List.of("T"));
+        jumpTable.put("(,S", List.of("-", "T"));
 
 
         // Переходы для <Выражения'>
         for (char op : operators) {
-            jumpTable.put(op + ",R", List.of("R", "S", "G"));
+            jumpTable.put(op + ",R", List.of("-", "R", "S", "G"));
         }
 
 
         // Переходы для <Выражение>
-        jumpTable.put("~,Q", List.of("R", "S"));
+        jumpTable.put("~,Q", List.of("-", "R", "S"));
         for (char c = 'a'; c <= 'z'; c++) {
-            jumpTable.put(c + ",Q", List.of("R", "S"));
+            jumpTable.put(c + ",Q", List.of("-", "R", "S"));
         }
         for (char c = '0'; c <= '9'; c++) {
-            jumpTable.put(c + ",Q", List.of("R", "S"));
+            jumpTable.put(c + ",Q", List.of("-", "R", "S"));
         }
-        jumpTable.put("(,Q", List.of("R", "S"));
+        jumpTable.put("(,Q", List.of("-", "R", "S"));
 
 
         // Переходы для <Программа>
-        jumpTable.put("VAR,I", List.of("K", "J"));
+        jumpTable.put("VAR,I", List.of("-", "K", "J"));
+
 
         // Переходы для <Список переменных>
         for (char c = 'a'; c <= 'z'; c++) {
-            jumpTable.put(c + ",L", List.of("M", "D"));
+            jumpTable.put(c + ",L", List.of("-", "M", "D"));
         }
 
 
         // Переходы для <Список операторов>
         String[] ops = {"READ", "CASE", "WRITE"};
         for (char c = 'a'; c <= 'z'; c++) {
-            jumpTable.put(c + ",O", List.of("O", "P"));
+            jumpTable.put(c + ",O", List.of("-", "O", "P"));
         }
         for (String op : ops) {
-            jumpTable.put(op + ",O", List.of("O", "P"));
+            jumpTable.put(op + ",O", List.of("-", "O", "P"));
         }
 
 
         // Переходы для <Присваивание>
         for (char c = 'a'; c <= 'z'; c++) {
-            jumpTable.put(c + ",N", List.of(";", "Q", "=", "D"));
+            jumpTable.put(c + ",N", List.of("-", ";", "Q", "=", "D"));
         }
 
 
+
         //3-ая группа
-        // Переходы для <Конст'>
-        jumpTable.put(":,C", List.of());
-        jumpTable.put("+,C", List.of());
-        jumpTable.put("-,C", List.of());
-        jumpTable.put("/,C", List.of());
+
+
+        // Переходы для <Конст>
+        jumpTable.put(":,C", List.of("-"));
+        jumpTable.put("+,C", List.of("-"));
+        jumpTable.put("-,C", List.of("-"));
+        jumpTable.put("/,C", List.of("-"));
 
 
         // Переходы для <Идент'>
-        jumpTable.put(",,E", List.of());
-        jumpTable.put("=,E", List.of());
-        jumpTable.put("+,E", List.of());
-        jumpTable.put("-,E", List.of());
-        jumpTable.put("/,E", List.of());
+        jumpTable.put(",,E", List.of("-"));
+        jumpTable.put("=,E", List.of("-"));
+        jumpTable.put("+,E", List.of("-"));
+        jumpTable.put("-,E", List.of("-"));
+        jumpTable.put("/,E", List.of("-"));
 
 
         // Переходы для <Список выбора'>
-        jumpTable.put("END_CASE,W", List.of());
+        jumpTable.put("END_CASE,W", List.of("-"));
 
 
         // Переходы для <Выражение'>
-        jumpTable.put(";,R", List.of());
-        jumpTable.put("),R", List.of());
-        jumpTable.put("OF,R", List.of());
-        jumpTable.put("+,R", List.of("R", "S", "G"));
-        jumpTable.put("-,R", List.of("R", "S", "G"));
-        jumpTable.put("/,R", List.of("R", "S", "G"));
+        jumpTable.put(";,R", List.of("-"));
+        jumpTable.put("),R", List.of("-"));
+        jumpTable.put("OF,R", List.of("-"));
+        jumpTable.put("+,R", List.of("-", "R", "S", "G"));
+        jumpTable.put("-,R", List.of("-", "R", "S", "G"));
+        jumpTable.put("/,R", List.of("-", "R", "S", "G"));
 
 
         // Переходы для <Список переменных'>
-        jumpTable.put(":,M", List.of());
-        jumpTable.put("),M", List.of());
+        jumpTable.put(":,M", List.of("-"));
+        jumpTable.put("),M", List.of("-"));
 
 
         // Переходы для <Список операторов>
-        jumpTable.put("END,O", List.of());
+        jumpTable.put("END,O", List.of("-"));
+
 
 
         //4-ая группа
-        jumpTable.put(":,:", List.of());
-        jumpTable.put(";,;", List.of());
-        jumpTable.put("OF,OF", List.of());
-        jumpTable.put("END_CASE,END_CASE", List.of());
-        jumpTable.put("(,(", List.of());
-        jumpTable.put("),)", List.of());
-        jumpTable.put("=,=", List.of());
-        jumpTable.put("INTEGER,INTEGER", List.of());
-        jumpTable.put("END,END", List.of());
+        jumpTable.put(":,:", List.of("+"));
+        jumpTable.put(";,;", List.of("+"));
+        jumpTable.put("OF,OF", List.of("+"));
+        jumpTable.put("END_CASE,END_CASE", List.of("+"));
+        jumpTable.put("(,(", List.of("+"));
+        jumpTable.put("),)", List.of("+"));
+        jumpTable.put("=,=", List.of("+"));
+        jumpTable.put("INTEGER,INTEGER", List.of("+"));
+        jumpTable.put("END,END", List.of("+"));
     }
 }
